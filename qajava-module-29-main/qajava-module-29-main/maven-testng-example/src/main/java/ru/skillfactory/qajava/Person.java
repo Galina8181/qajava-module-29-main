73e65faf8ea86b9fe541732101f3d6dab48b8953
Galina Mamaeva <orang811@icloud.com>
2021-05-20 15:22:20 +0300

    Проект 29.3

---
+++ qajava-module-29-main/qajava-module-29-main/maven-testng-example/src/main/java/ru/skillfactory/qajava/Person.java
@@ -0,0 +1,13 @@
+
+package ru.skillfactory.qajava;
+
+public class Person {
+
+
+    public static boolean isTeenager(int age) {
+        boolean result;
+        result = age < 19;
+        return result;
+    }
+
+}
